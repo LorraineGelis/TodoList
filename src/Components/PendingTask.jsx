21a9415aef056c31610a1d749d694596b11b487b
Lorraine Gelis <lgelisdiaz@outlook.com>
2022-02-18 13:15:05 -0500

    PendingTask component creation

---
+++ src/Components/PendingTask.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const PendingTask = (props) => {
+
+    return(
+    <h1 className='bg-text-primary text-white text-center p-4'>{props.userName} To-do Task Pending: {props.task.filter(e => !e.done).length} </h1>
+    )
+}
+
