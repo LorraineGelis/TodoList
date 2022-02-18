be0e27ce763d3e2bedff269acbe2800817a88cf8
Lorraine Gelis <lgelisdiaz@outlook.com>
2022-02-18 13:36:11 -0500

    PendingTask componente class is modified

--- src/Components/PendingTask.jsx
+++ src/Components/PendingTask.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const PendingTask = (props) => {
 
     return(
-    <h1 className='bg-text-primary text-white text-center p-4'>{props.userName} To-do Task Pending: {props.task.filter(e => !e.done).length} </h1>
+    <h1 className='bg-primary text-white text-center p-4'>{props.userName} To-do Task Pending: {props.task.filter(e => !e.done).length} </h1>
     )
 }
 
