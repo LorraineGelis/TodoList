40a5e44bebcd28e85cbc6cc72076cd114855efee
Lorraine Gelis <lgelisdiaz@outlook.com>
2022-02-18 19:06:53 -0500

    localStorage method is implemented for the parent component

--- src/Components/ListComponent.jsx
+++ src/Components/ListComponent.jsx
@@ -2,22 +2,27 @@
 import React from 'react';
 import { TaskRow } from './TaskRow.jsx';
 import  {PendingTask} from './PendingTask.jsx';
+import {TaskCreator} from './TaskCreator.jsx'
+import {TaskDone} from './TaskDone.jsx'
 
 
 
-export const ListComponents = ({task, toggleTask, userName}) => {
+export const ListComponents = ({task, toggleTask, userName, createNewTask, cheked, changetask, taskdone}) => {
   
  
-    const taskTableRows = () => {
-     return task.map((tasks) => (
+    const taskTableRows = (doneVlr) => {
+     return task
+     .filter(task => task.done === doneVlr)     
+     .map((tasks) => (
           <TaskRow task={tasks} key={tasks.name} toggleTask={toggleTask} />
       ))
   
-  }  
+  } 
   return(
       
       <div>
       <PendingTask userName={userName} task={task}></PendingTask>
+      <TaskCreator createNewTask={createNewTask}></TaskCreator> 
       <table className="table table-striped table-bordered">
         <thead>
           <tr>
@@ -25,8 +30,29 @@ export const ListComponents = ({task, toggleTask, userName}) => {
             <th>Done</th>
           </tr>
         </thead>
-        <tbody>{taskTableRows()}</tbody>
+        <tbody>{taskTableRows(false)}</tbody>
       </table>
+      <div className="bg-secondary-text-white text-center p-2">
+        <TaskDone
+        title={"Show Task Done"}
+        cheked={cheked}
+        changetask={changetask}
+        ></TaskDone>
+      </div>
+      {cheked && (
+        <table className="table table-striped table-bordered">
+          <thead>
+            <tr>
+              <th>Done</th>
+              <th>Description</th>
+            </tr>
+          </thead>
+          <tbody>
+            {taskTableRows(true)}
+          </tbody>
+        </table>
+      )
+      }
     </div>
   )
   
--- src/Components/ListContainer.jsx
+++ src/Components/ListContainer.jsx
@@ -1,27 +1,55 @@
 
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {ListComponents} from './ListComponent.jsx';
 
 const ListContainer = () => {
 
     
     const [userName, SetUserName] = useState('Lore')
-    const [taskItems, setTaskItems] = useState([
+    const [taskItems, setTaskItems] = useState([]);
 
-        { name: "task One", done: false },
-        { name: "task two", done: false },
-        { name: "task three", done: true },
-        { name: "task four", done: false },
-      ]);
+      const [showTaskDone, setShowTaskDone ] = useState(true)
+
+      useEffect(() => {
+        let dataTask = localStorage.getItem('task');
+        if(dataTask != null){
+          setTaskItems(JSON.parse(dataTask));
+        }else{
+          SetUserName("Welcome")
+          setTaskItems([
+            { name: "Learn type Script", done: false },
+            { name: "Learn Angular", done: true },
+          ])
+          setShowTaskDone(true);
+        }
+
+      }, []);
+
+      useEffect(() => {
+        localStorage.setItem('task', JSON.stringify(taskItems));
+      }, [taskItems])
+      
 
        const toggleTask = task => {
          //console.log("funciono yuju")
          setTaskItems(taskItems.map(e => (e.name === task.name ? {...e, done: !e.done} : e))) 
        }
-        
+
+       const createNewTask = (newTaskName) => {
+        if(!taskItems.find(e => e.name === newTaskName)){
+          setTaskItems([...taskItems, {name: newTaskName, done: false}])
+        }
+       }
+
       
       
-      return <ListComponents task= {taskItems} toggleTask={toggleTask} userName={userName} />
+      return <ListComponents
+       task= {taskItems} 
+       toggleTask={toggleTask} 
+       userName={userName} 
+       createNewTask={createNewTask}
+        cheked={showTaskDone} 
+        changetask={checked => setShowTaskDone(checked)} />
 }
 
 export default ListContainer;
